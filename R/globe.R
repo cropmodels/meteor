befaf1362099bd5a2ad647d0f994961c1bf8d6a4
rhijmans <r.hijmans@gmail.com>
2023-01-01 14:06:50 -0800

    x

--- R/globe.R
+++ R/globe.R
@@ -32,19 +32,19 @@ setMethod("Tg", signature(x="SpatRasterDataset"),
 		nc <- ncol(r)
 		out <- terra::rast(r)
 		terra::readStart(x)
+		on.exit(terra::readStop(x))
 		b <- terra::writeStart(out, filename, overwrite, wopt=list(...), n=5, sources=terra::sources(x))
 		for (i in 1:b$n) {
-			temp <- as.vector(t(terra::readValues(x$temp, b$row[i], b$nrows[i], 1, nc, mat=TRUE)))
-			rhum <- as.vector(t(terra::readValues(x$rhum, b$row[i], b$nrows[i], 1, nc, mat=TRUE)))
-			srad <- as.vector(t(terra::readValues(x$srad, b$row[i], b$nrows[i], 1, nc, mat=TRUE)))
-			wind <- as.vector(t(terra::readValues(x$wind, b$row[i], b$nrows[i], 1, nc, mat=TRUE)))
+			temp <- terra::readValues(x$temp, b$row[i], b$nrows[i], 1, nc, mat=TRUE)
+			rhum <- terra::readValues(x$rhum, b$row[i], b$nrows[i], 1, nc, mat=TRUE)
+			srad <- terra::readValues(x$srad, b$row[i], b$nrows[i], 1, nc, mat=TRUE)
+			wind <- terra::readValues(x$wind, b$row[i], b$nrows[i], 1, nc, mat=TRUE)
 			c1 <- (b$row[i]-1) * nc + 1 
-			c2 <- (b$row[i]+b$nrows[i]-1) * nc + 1 
+			c2 <- (b$row[i]+b$nrows[i]-1) * nc 
 			lat <- terra::yFromCell(r, c1:c2)
 			tg <- .Tg2(temp, rhum, wind, srad, year, doy, lat)
 			terra::writeValues(out, tg, b$row[i], b$nrows[i])
 		}
-		terra::readStop(x)
 		terra::writeStop(out)
 	}
 )
@@ -61,6 +61,40 @@ setMethod("Tnwb", signature(x="data.frame"),
 	}
 )
 
+
+setMethod("Tnwb", signature(x="SpatRasterDataset"),
+	function(x, filename="", overwrite=FALSE, ...) {
+		if (!all(c("temp", "rhum", "wind", "srad") %in% names(x))) {
+			stop("x must have variables 'temp', 'rhum', 'wind', 'srad'")		
+		}
+		date <- terra::time(x$temp)
+		if (any(is.na(date))) {
+			stop("x$temp does not have valid time stamps")
+		}
+		year <- fromDate(date, "year")
+		doy <- fromDate(date, "doy")
+		r <- terra::rast(x$temp)
+		nc <- ncol(r)
+		out <- terra::rast(r)
+		terra::readStart(x)
+		on.exit(terra::readStop(x))
+		b <- terra::writeStart(out, filename, overwrite, wopt=list(...), n=5, sources=terra::sources(x))
+		for (i in 1:b$n) {
+			temp <- terra::readValues(x$temp, b$row[i], b$nrows[i], 1, nc, mat=TRUE)
+			rhum <- terra::readValues(x$rhum, b$row[i], b$nrows[i], 1, nc, mat=TRUE)
+			srad <- terra::readValues(x$srad, b$row[i], b$nrows[i], 1, nc, mat=TRUE)
+			wind <- terra::readValues(x$wind, b$row[i], b$nrows[i], 1, nc, mat=TRUE)
+			c1 <- (b$row[i]-1) * nc + 1 
+			c2 <- (b$row[i]+b$nrows[i]-1) * nc 
+			lat <- terra::yFromCell(r, c1:c2)
+			tnwb <- .Tnwb2(temp, rhum, wind, srad, year, doy, lat, TRUE)
+			terra::writeValues(out, tnwb, b$row[i], b$nrows[i])
+		}
+		terra::writeStop(out)
+	}
+)
+
+
 setMethod("WBGT", signature(x="data.frame"),
 	function(x, latitude) {
 		if (!all(c("temp", "rhum", "wind", "srad", "date") %in% names(x))) {
@@ -71,3 +105,37 @@ setMethod("WBGT", signature(x="data.frame"),
 		.Tnwb1(x$temp, x$rhum, x$wind, x$srad, year, doy, latitude, FALSE)
 	}
 )
+
+
+setMethod("WBGT", signature(x="SpatRasterDataset"),
+	function(x, filename="", overwrite=FALSE, ...) {
+		if (!all(c("temp", "rhum", "wind", "srad") %in% names(x))) {
+			stop("x must have variables 'temp', 'rhum', 'wind', 'srad'")		
+		}
+		date <- terra::time(x$temp)
+		if (any(is.na(date))) {
+			stop("x$temp does not have valid time stamps")
+		}
+		year <- fromDate(date, "year")
+		doy <- fromDate(date, "doy")
+		r <- terra::rast(x$temp)
+		nc <- ncol(r)
+		out <- terra::rast(r)
+		terra::readStart(x)
+		on.exit(terra::readStop(x))
+		b <- terra::writeStart(out, filename, overwrite, wopt=list(...), n=5, sources=terra::sources(x))
+		for (i in 1:b$n) {
+			temp <- terra::readValues(x$temp, b$row[i], b$nrows[i], 1, nc, mat=TRUE)
+			rhum <- terra::readValues(x$rhum, b$row[i], b$nrows[i], 1, nc, mat=TRUE)
+			srad <- terra::readValues(x$srad, b$row[i], b$nrows[i], 1, nc, mat=TRUE)
+			wind <- terra::readValues(x$wind, b$row[i], b$nrows[i], 1, nc, mat=TRUE)
+			c1 <- (b$row[i]-1) * nc + 1 
+			c2 <- (b$row[i]+b$nrows[i]-1) * nc 
+			lat <- terra::yFromCell(r, c1:c2)
+			tnwb <- .Tnwb2(temp, rhum, wind, srad, year, doy, lat, FALSE)
+			terra::writeValues(out, tnwb, b$row[i], b$nrows[i])
+		}
+		terra::writeStop(out)
+	}
+)
+
--- R/pwc.R
+++ R/pwc.R
@@ -2,38 +2,38 @@
 # License GPL3
 
 if (!isGeneric("pwc")) {setGeneric("pwc", function(x, ...) standardGeneric("pwc"))}
-if (!isGeneric("ucti")) {setGeneric("ucti", function(x, ...) standardGeneric("ucti"))}
+if (!isGeneric("utci")) {setGeneric("utci", function(x, ...) standardGeneric("utci"))}
 
 
 setMethod("pwc", signature(x="numeric"),
 	function(x, input="wbgt") {
-		input = match.arg(tolower(input), c("wbgt", "ucti"))
+		input = match.arg(tolower(input), c("wbgt", "utci"))
 		if (input == "wbgt") {
 			.pwc_wbgt(x)
 		} else {
-			.pwc_ucti(x)		
+			.pwc_utci(x)		
 		}
 	}
 )
 
 setMethod("pwc", signature(x="SpatRaster"),
 	function(x, input="wbgt", filename="", overwrite=FALSE, ...) {
-		input = match.arg(tolower(input), c("wbgt", "ucti"))
+		input = match.arg(tolower(input), c("wbgt", "utci"))
 		if (input == "wbgt") {
-			app(x, .pwc_wbgt, filename=filename, overwrite=overwrite, wopt=list(...))
+			terra::app(x, .pwc_wbgt, filename=filename, overwrite=overwrite, wopt=list(...))
 		} else {
-			app(x, .pwc_ucti, filename=filename, overwrite=overwrite, wopt=list(...))
+			terra::app(x, .pwc_utci, filename=filename, overwrite=overwrite, wopt=list(...))
 		}
 	}
 )
 
 
-setMethod("ucti", signature(x="data.frame"),
-	function(x, input="wbgt") {
-		if (!all(c("temp", "rhum", "tglb", "wind", "rhum") %in% names(x))) {
+setMethod("utci", signature(x="data.frame"),
+	function(x) {
+		if (!all(c("temp", "rhum", "tglb", "wind") %in% names(x))) {
 			stop("x must have variables 'temp', 'rhum', 'wind', and 'tglb'")		
 		}
-		.ucti(x$temp, x$tglb, x$wind, x$rhum)
+		.utci(x$temp, x$tglb, x$wind, x$rhum)
 	}
 )
 
